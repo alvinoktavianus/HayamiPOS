d24af739dec526fcf42dae1784acae220edbdac1
Alvin Oktavianus <agentsam1995@gmail.com>
2017-08-27 16:25:34 +0700

    Create backend configuration

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -36,8 +36,11 @@ export class LoginPage {
     let loading = this.loadingCtrl.create({
       content: 'Please wait...'
     });
+    const hostUrl = localStorage.getItem('hostUrl');
 
-    if (this.email == null || this.password == null) {
+    if (!hostUrl) {
+      this.presentNotConfigured();
+    } else if (this.email == null || this.password == null) {
       this.presentAlert();
     } else {
       let credentials = {
@@ -47,9 +50,11 @@ export class LoginPage {
 
       console.log(credentials);
 
-      loading.present();
+      // loading.present();
 
-      loading.dismiss();
+      // loading.dismiss();
+
+      this.navCtrl.setRoot(TabsPage);
     }
 
   }
@@ -61,6 +66,66 @@ export class LoginPage {
       buttons: ['OK']
     });
     alert.present();
+  }
+
+  askConfirmation() {
+    let confirm = this.alertCtrl.create({
+      title: 'Configure backend?',
+      message: 'Are you sure you want to configure backend? This action cannot be undone.',
+      buttons: [
+        {
+          text: 'No'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.showConfigurationSettings();
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  showConfigurationSettings() {
+    let confirm = this.alertCtrl.create({
+      title: 'Configure backend',
+      message: 'Enter backend host and port',
+      inputs: [
+        {
+          name: 'hostUrl',
+          placeholder: 'Backend host and port'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Save',
+          handler: data => {
+            localStorage.setItem('hostUrl', data.hostUrl);
+            this.showSuccess();
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  showSuccess() {
+    let alert = this.alertCtrl.create({
+      title: 'Saved',
+      subTitle: 'Successfully saved configuration!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  presentNotConfigured() {
+    let notConfigured = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Please configure backend first!',
+      buttons: ['OK']
+    });
+    notConfigured.present();
   }
 
 }
