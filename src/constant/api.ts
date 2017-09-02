fd31177f43023bbaecf8a9f2498521256b92ad42
Alvin Oktavianus <agentsam1995@gmail.com>
2017-09-02 12:24:16 +0700

    Get all of product from backend

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,3 +1,12 @@
 import {ENV} from "./environment";
+import {Headers} from "@angular/http";
 
 export const LOGIN = `${ENV.API_HOST}/api/users/login`;
+
+export const PRODUCTS = `${ENV.API_HOST}/api/products`;
+
+export function REQUEST_HEADERS() {
+  let headers = new Headers();
+  headers.append('TOKEN', localStorage.getItem('TOKEN'));
+  return headers;
+}
