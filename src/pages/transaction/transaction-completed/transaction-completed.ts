f0e987d206257d81247bef9671e4c80851457254
Alvin Oktavianus <agentsam1995@gmail.com>
2017-09-11 01:06:29 +0700

    Add number formatter and finished up transaction completed

--- src/pages/transaction/transaction-completed/transaction-completed.ts
+++ src/pages/transaction/transaction-completed/transaction-completed.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Http} from "@angular/http";
+import {REQUEST_HEADERS, TRANSACTIONS} from "../../../constant/api";
 
 /**
  * Generated class for the TransactionCompletedPage page.
@@ -15,11 +17,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TransactionCompletedPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  transactions: any = [];
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public http: Http) {
+    this.fetchAllData();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TransactionCompletedPage');
+  }
+
+  fetchAllData() {
+    this.http
+      .get(TRANSACTIONS, {headers: REQUEST_HEADERS()})
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          data.forEach(tr => {
+            if (tr.FgStatus == 'O') this.transactions.push(tr);
+          });
+        }
+      );
   }
 
 }
