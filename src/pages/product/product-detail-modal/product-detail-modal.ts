4f6e5906e0025127cb40f3a19d4919af8aeab397
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-09-16 07:01:14 +0700

    Modal Display (masih error)

--- src/pages/product/product-detail-modal/product-detail-modal.ts
+++ src/pages/product/product-detail-modal/product-detail-modal.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {REQUEST_HEADERS, TYPES, MODELS} from "../../../constant/api";
+import {Http} from "@angular/http";
+import {map} from "rxjs/operator/map";
 
 /**
  * Generated class for the ProductDetailModalPage page.
@@ -16,10 +19,41 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 export class ProductDetailModalPage {
 
   product: object = {};
-
+  types: object = {};
+  models: object = {};
   constructor(public navCtrl: NavController,
-              public navParams: NavParams) {
+              public navParams: NavParams,
+              public http: Http) {
     this.product = {...navParams.data};
+    this.fetchData();
   }
 
+  fetchData() {
+    this.http
+      .get(TYPES, {headers: REQUEST_HEADERS()})
+      .map(res => res.json())
+      .subscribe(
+        data => {
+          data.forEach((typ) => {
+            this.types[typ.TypeId] = typ;
+          });
+        }
+      );
+
+    this.http
+      .get(MODELS, {headers: REQUEST_HEADERS()})
+      .map(res => res.json())
+        .subscribe(
+          data => {
+            data.forEach((mod) => {
+              this.models[mod.ModelId] = mod;
+            });
+          }
+        );
+      }
+
+  doRefresh(refresher) {
+    this.fetchData();
+    refresher.complete();
+  }
 }
